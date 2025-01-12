b53843c2ada160bcf2f9d10402aa43ee52ad6301
Sanjeevkrmenon <sanjeev.krishna.menon@gmail.com>
2025-01-12 15:43:03 +0000

    added many things of get api serialer etc

---
+++ profiles_api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from profiles_api import serializers  # Add this import statement
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
